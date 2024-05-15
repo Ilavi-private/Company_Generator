264f48f4d0d95be53ace419b5bca08a6c804af70
Ilavi <83451931+Ilavi-private@users.noreply.github.com>
2024-05-15 23:48:22 +0300

    feat: Add tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.angelicahelp"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.angelicahelp"
